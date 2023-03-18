e9ed2ad806ad4800f9d66ac82d1222f22d7f655a
euclides <euclideskinto@gmail.com>
2023-03-18 15:36:51 -0300

    Add userResource

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\v1\UsersResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Response;
 
 class UserController extends Controller
@@ -14,17 +16,17 @@ class UserController extends Controller
     {
         $this->user = $user;
     }
+
     /**
      * Display a listing of the resource.
+     * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
         $users = $this->user->query()
             ->orderBy('created_at', 'desc')
             ->paginate(10);
-        $statusCode = Response::HTTP_OK;
-        $response = ['data' => $users,];
-        return response()->json($response, $statusCode);
+        return UsersResource::collection($users);
     }
 
     /**
--- tests/Feature/Api/v1/UserTest.php
+++ tests/Feature/Api/v1/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Api\v1;
 
+use Illuminate\Testing\Fluent\AssertableJson;
 use Illuminate\Foundation\Testing\{RefreshDatabase, WithFaker};
 use Tests\TestCase;
 
@@ -13,21 +14,81 @@ class UserTest extends TestCase
      * A basic feature test example.
      */
     /** @test */
-    public function get_users_index(): void
+    public function if_get_users_all_index(): void
     {
         $data = [
             'name' => $this->faker->name,
             'email' => $this->faker->email,
-            'password' => 'password123',
-            'password_confirmation' => 'password123',
+            'password' => 'pontue123',
+            'password_confirmation' => 'pontue123',
         ];
+        $res = $this->post('/api/auth/register', $data);
 
-        $response = $this->post('/api/auth/register', $data);
-
-        $token = $response['token'];
+        $token = $res['token'];
         $response = $this->withHeaders([
             'Authorization' => "Bearer $token",
         ])->getJson('/api/users');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function if_get_users_all_type_index(): void
+    {
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => 'pontue123',
+            'password_confirmation' => 'pontue123',
+        ];
+
+        $res = $this->post('/api/auth/register', $data);
+
+        $token = $res['token'];
+        $response = $this->withHeaders([
+            'Authorization' => "Bearer $token",
+        ])->getJson('/api/users');
+//        dd($response['data']['data']);
+
+        $response->assertJson(fn (AssertableJson $json)=>
+            $json->whereAllType([
+                'data.data.0.id' => 'integer',
+                'data.data.0.name' => 'string',
+                'data.data.0.email' => 'string',
+                'data.data.0.email_verified_at' => 'string|null',
+                'data.data.0.created_at' => 'string',
+                'data.data.0.updated_at' => 'string',
+            ])
+        );
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function should_get_users_all_type_index(): void
+    {
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => 'pontue123',
+            'password_confirmation' => 'pontue123',
+        ];
+
+        $res = $this->post('/api/auth/register', $data);
+
+        $token = $res['token'];
+        $response = $this->withHeaders([
+            'Authorization' => "Bearer $token",
+        ])->getJson('/api/users');
+//        dd($response['data']['data']);
+
+        $response->assertJson(fn (AssertableJson $json)=>
+        $json->whereAllType([
+            'data.data.0.id' => 'integer',
+            'data.data.0.name' => 'string',
+            'data.data.0.email' => 'string',
+        ])
+        );
 
         $response->assertStatus(200);
     }
